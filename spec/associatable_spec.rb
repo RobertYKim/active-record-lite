39acd7cdb406fbf13ab4852e94b3f10df821ae19
RobertYKim <rob.y.kim@gmail.com>
2016-02-27 20:19:45 -0500

    Update associatable test.

--- spec/associatable_spec.rb
+++ spec/associatable_spec.rb
@@ -25,29 +25,29 @@ describe 'AssocOptions' do
 
   describe 'HasManyOptions' do
     it 'provides defaults' do
-      options = HasManyOptions.new('cats', 'Human')
+      options = HasManyOptions.new('dogs', 'Human')
 
       expect(options.foreign_key).to eq(:human_id)
-      expect(options.class_name).to eq('Cat')
+      expect(options.class_name).to eq('Dog')
       expect(options.primary_key).to eq(:id)
     end
 
     it 'allows overrides' do
-      options = HasManyOptions.new('cats', 'Human',
+      options = HasManyOptions.new('dogs', 'Human',
                                    foreign_key: :owner_id,
-                                   class_name: 'Kitten',
+                                   class_name: 'Puppy',
                                    primary_key: :human_id
       )
 
       expect(options.foreign_key).to eq(:owner_id)
-      expect(options.class_name).to eq('Kitten')
+      expect(options.class_name).to eq('Puppy')
       expect(options.primary_key).to eq(:human_id)
     end
   end
 
   describe 'AssocOptions' do
     before(:all) do
-      class Cat < SQLObject
+      class Dog < SQLObject
         self.finalize!
       end
 
@@ -62,16 +62,16 @@ describe 'AssocOptions' do
       options = BelongsToOptions.new('human')
       expect(options.model_class).to eq(Human)
 
-      options = HasManyOptions.new('cats', 'Human')
-      expect(options.model_class).to eq(Cat)
+      options = HasManyOptions.new('dogs', 'Human')
+      expect(options.model_class).to eq(Dog)
     end
 
     it '#table_name returns table name of associated object' do
       options = BelongsToOptions.new('human')
       expect(options.table_name).to eq('humans')
 
-      options = HasManyOptions.new('cats', 'Human')
-      expect(options.table_name).to eq('cats')
+      options = HasManyOptions.new('dogs', 'Human')
+      expect(options.table_name).to eq('dogs')
     end
   end
 end
@@ -81,7 +81,7 @@ describe 'Associatable' do
   after(:each) { DBConnection.reset }
 
   before(:all) do
-    class Cat < SQLObject
+    class Dog < SQLObject
       belongs_to :human, foreign_key: :owner_id
 
       finalize!
@@ -90,7 +90,7 @@ describe 'Associatable' do
     class Human < SQLObject
       self.table_name = 'humans'
 
-      has_many :cats, foreign_key: :owner_id
+      has_many :dogs, foreign_key: :owner_id
       belongs_to :house
 
       finalize!
@@ -104,62 +104,62 @@ describe 'Associatable' do
   end
 
   describe '#belongs_to' do
-    let(:breakfast) { Cat.find(1) }
-    let(:devon) { Human.find(1) }
+    let(:spot) { Dog.find(1) }
+    let(:peter) { Human.find(1) }
 
-    it 'fetches `human` from `Cat` correctly' do
-      expect(breakfast).to respond_to(:human)
-      human = breakfast.human
+    it 'fetches `human` from `Dog` correctly' do
+      expect(spot).to respond_to(:human)
+      human = spot.human
 
       expect(human).to be_instance_of(Human)
-      expect(human.fname).to eq('Devon')
+      expect(human.fname).to eq('Peter')
     end
 
     it 'fetches `house` from `Human` correctly' do
-      expect(devon).to respond_to(:house)
-      house = devon.house
+      expect(peter).to respond_to(:house)
+      house = peter.house
 
       expect(house).to be_instance_of(House)
-      expect(house.address).to eq('26th and Guerrero')
+      expect(house.address).to eq('432 Park Ave')
     end
 
     it 'returns nil if no associated object' do
-      stray_cat = Cat.find(5)
-      expect(stray_cat.human).to eq(nil)
+      stray_dog = Dog.find(5)
+      expect(stray_dog.human).to eq(nil)
     end
   end
 
   describe '#has_many' do
-    let(:ned) { Human.find(3) }
-    let(:ned_house) { House.find(2) }
+    let(:peter) { Human.find(3) }
+    let(:peter_house) { House.find(2) }
 
-    it 'fetches `cats` from `Human`' do
-      expect(ned).to respond_to(:cats)
-      cats = ned.cats
+    it 'fetches `dogs` from `Human`' do
+      expect(peter).to respond_to(:dogs)
+      dogs = peter.dogs
 
-      expect(cats.length).to eq(2)
+      expect(dogs.length).to eq(2)
 
-      expected_cat_names = %w(Haskell Markov)
+      expected_dog_names = %w(Lassie Pluto)
       2.times do |i|
-        cat = cats[i]
+        dog = dogs[i]
 
-        expect(cat).to be_instance_of(Cat)
-        expect(cat.name).to eq(expected_cat_names[i])
+        expect(dog).to be_instance_of(Dog)
+        expect(dog.name).to eq(expected_dog_names[i])
       end
     end
 
     it 'fetches `humans` from `House`' do
-      expect(ned_house).to respond_to(:humans)
-      humans = ned_house.humans
+      expect(peter_house).to respond_to(:humans)
+      humans = peter_house.humans
 
       expect(humans.length).to eq(1)
       expect(humans[0]).to be_instance_of(Human)
-      expect(humans[0].fname).to eq('Ned')
+      expect(humans[0].fname).to eq('Jonathan')
     end
 
     it 'returns an empty array if no associated items' do
-      catless_human = Human.find(4)
-      expect(catless_human.cats).to eq([])
+      dogless_human = Human.find(4)
+      expect(dogless_human.dogs).to eq([])
     end
   end
 
@@ -172,8 +172,8 @@ describe 'Associatable' do
     end
 
     it 'stores `belongs_to` options' do
-      cat_assoc_options = Cat.assoc_options
-      human_options = cat_assoc_options[:human]
+      dog_assoc_options = Dog.assoc_options
+      human_options = dog_assoc_options[:human]
 
       expect(human_options).to be_instance_of(BelongsToOptions)
       expect(human_options.foreign_key).to eq(:owner_id)
@@ -182,34 +182,34 @@ describe 'Associatable' do
     end
 
     it 'stores options separately for each class' do
-      expect(Cat.assoc_options).to have_key(:human)
+      expect(Dog.assoc_options).to have_key(:human)
       expect(Human.assoc_options).to_not have_key(:human)
 
       expect(Human.assoc_options).to have_key(:house)
-      expect(Cat.assoc_options).to_not have_key(:house)
+      expect(Dog.assoc_options).to_not have_key(:house)
     end
   end
 
   describe '#has_one_through' do
     before(:all) do
-      class Cat
+      class Dog
         has_one_through :home, :human, :house
 
         self.finalize!
       end
     end
 
-    let(:cat) { Cat.find(1) }
+    let(:dog) { Dog.find(1) }
 
     it 'adds getter method' do
-      expect(cat).to respond_to(:home)
+      expect(dog).to respond_to(:home)
     end
 
-    it 'fetches associated `home` for a `Cat`' do
-      house = cat.home
+    it 'fetches associated `home` for a `Dog`' do
+      house = dog.home
 
       expect(house).to be_instance_of(House)
-      expect(house.address).to eq('26th and Guerrero')
+      expect(house.address).to eq('432 Park Ave')
     end
   end
 end
